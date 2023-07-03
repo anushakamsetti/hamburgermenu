ff90ce75e766de1754cf4b5f792bec20a22b8e33
anushakamsetti <annu.honey97@gmail.com>
2023-07-03 08:18:07 +0000

    first

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,21 @@
+import Header from '../Header'
+
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <Header />
+    <div className="not-found-content-container">
+      <div className="not-found-responsive-container">
+        <img className="image" src="" alt="not found" />
+        <h1 className="heading">Lost Your Way?</h1>
+        <p className="description">
+          Sorry,we cannot find that page. You will find lots to explore on the
+          home page
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default NotFound
